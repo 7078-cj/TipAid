88b42213cdaae2c30e4a31cf451743edd219fb2f
rshdgvn <rasheedgavinesponga@gmail.com>
2025-12-03 17:29:26 +0800

    fix: form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import RecipePage from "./pages/form/RecipePage";
-import PeoplePage from "./pages/form/PeoplePage";
-import BudgetPage from "./pages/form/BudgetPage";
-import AddressPage from "./pages/form/AddressPage";
-import SummaryPage from "./pages/form/SummaryPage";
+import FormPage from "./pages/FormPage";
+import SummaryPage from "./pages/SummaryPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/recipe" element={<RecipePage />} />
-        <Route path="/people" element={<PeoplePage />} />
-        <Route path="/budget" element={<BudgetPage />} />
-        <Route path="/address" element={<AddressPage />} />
+        <Route path="/form" element={<FormPage />} />
         <Route path="/summary" element={<SummaryPage />} />
       </Routes>
     </Router>
